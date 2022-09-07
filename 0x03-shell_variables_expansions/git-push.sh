3377cfb81447dcc3ae48ad7005e5811a7bf98225
efodthe2nd <davidefod@gmail.com>
2022-09-07 23:18:05 +0000

    "fix script"

--- 0x03-shell_variables_expansions/git-push.sh
+++ 0x03-shell_variables_expansions/git-push.sh
@@ -7,4 +7,9 @@ if [ -n "$(git status - porcelain)" ]; then
 	git status
 	echo "Pushing data to remote server!!!"
 	git push
+fi
+if [ "$(git status --porcelain)" ]; then
+	echo "There are no files to commit, stage or push"
+fi
+
 
